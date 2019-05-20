df8a942a01213d182f62d69ecaa802acf1db0de6
Isaac Friedman <isaac@isaac-friedman.com>
2019-05-19 21:49:54 -0400

    Fixed typo in import

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -2,7 +2,7 @@
 
 import http.server
 import threading
-from socketserver import ThreadingMixin
+from socketserver import ThreadingMixIn
 import requests
 from urllib.parse import unquote, parse_qs
 import os
